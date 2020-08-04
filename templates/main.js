40bb592b8d3d8f6d9425d2f59081bfcb29a60889
ryanburgess <burgessdryan@gmail.com>
2020-08-04 14:22:45 -0700

    fix episode canonical urls

--- templates/main.js
+++ templates/main.js
@@ -4,12 +4,13 @@ const header = require('./header');
 const footer = require('./footer');
 const strings = require('../content/strings.json');
 
-module.exports = function main(pageType, content, title, desc) {
+module.exports = function main(pageType, content, title, desc, link) {
   'use strict';
   let heading = title;
   let pageContent;
   let pageTitle;
   let pageOG;
+  let urlCanonical = '';
   let path;
   let css;
   let js;
@@ -36,6 +37,7 @@ module.exports = function main(pageType, content, title, desc) {
     css = `public/css/episode.css?v=${cssVersion}`;
     pageContent = content;
     pageTitle = title + ' - ';
+    urlCanonical = `episodes/${link}`;
     js = '';
     //js = '<script src="../../public/js/min/episode.min.js" type="text/javascript"></script>';
   }
@@ -61,7 +63,7 @@ module.exports = function main(pageType, content, title, desc) {
                   <meta property="og:image"
                    content="${pageOG}">
                   <link rel="icon" href="https://managerstable.com/favicon.ico" type="image/x-icon">
-                  <link rel="canonical" href="https://managerstable.com/">
+                  <link rel="canonical" href="https://managerstable.com/${urlCanonical}">
                   <link rel="stylesheet" href="${path}${css}" type="text/css" media="screen">
                   ${ga('UA-127809400-1')}
               </head>
